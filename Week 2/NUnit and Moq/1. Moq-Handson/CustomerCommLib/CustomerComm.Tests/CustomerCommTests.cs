634280fa3c3e80c074a96bd135cf5459e850d632
Smruti Gansalvesh <smrutigansalvesh17@gmail.com>
2025-06-29 13:01:44 +0530

    Added Week 2 files

---
+++ Week 2/NUnit and Moq/1. Moq-Handson/CustomerCommLib/CustomerComm.Tests/CustomerCommTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> _mockMailSender = null!;
+        private CustomerCommLib.CustomerComm _customerComm = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _mockMailSender = new Mock<IMailSender>();
+            _mockMailSender
+                .Setup(m => m.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            _customerComm = new CustomerCommLib.CustomerComm(_mockMailSender.Object);
+        }
+
+        [Test]
+        public void SendMailToCustomer_ShouldReturnTrue()
+        {
+            var result = _customerComm.SendMailToCustomer();
+            Assert.That(result, Is.True);
+            _mockMailSender.Verify(
+                m => m.SendMail(It.IsAny<string>(), It.IsAny<string>()),
+                Times.Once);
+        }
+    }
+}
