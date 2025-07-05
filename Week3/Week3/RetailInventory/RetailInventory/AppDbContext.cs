f3d0ea18db055b89dcb98b255829a83622eccf6a
Smruti Gansalvesh <smrutigansalvesh17@gmail.com>
2025-07-06 00:52:53 +0530

    Added Week 3 files

---
+++ Week3/Week3/RetailInventory/RetailInventory/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=smruti_database;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Packet Size=4096;Application Name=\"SQL Server Management Studio\";Command Timeout=0");
+    }
+}
