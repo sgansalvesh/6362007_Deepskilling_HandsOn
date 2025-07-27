00fcd41937ce360539857e28e63e7cdcf22d3e73
Smruti Gansalvesh <smrutigansalvesh17@gmail.com>
2025-07-27 19:17:10 +0530

    week 6 push

---
+++ Week6/4.ReactJS-HOL-blogapp/blogapp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Posts from './posts';
+
+function App() {
+  return (
+    <div className="App"
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <h1 style={{ textAlign: 'center'}}>Blog App</h1>
+      <Posts />
+    </div>
+  );
+}
+// By Smruti Gansalvesh
+export default App;
